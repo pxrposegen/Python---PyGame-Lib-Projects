2baf112c0267f0f3aca531ccafb0b5554b5cbeae
Genny <christo26thdec@gmail.com>
2024-12-22 22:17:18 +0530

    Created a class ImageObject to simplify the recurring usage of Image Surface and FRectangle.

--- SpaceWars.py
+++ SpaceWars.py
@@ -1,6 +1,7 @@
 import pygame
 from os.path import join
 from random import randint
+from ImageObject import ImageObject
 
 pygame.init()
 
@@ -13,27 +14,27 @@ running = True
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("SpaceWars")
 
-# Icon
+#Icon
 icon_path = join("assets", "bronze.png")
 icon_surface = pygame.image.load(icon_path).convert_alpha()
 pygame.display.set_icon(icon_surface)
 
-# Background Image
-background_image = pygame.image.load(join("assets", "Background.png")).convert_alpha()
-background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
-background_image_rect = background_image.get_frect(center = (WINDOW_WIDTH / 2,WINDOW_HEIGHT / 2))
+#Background Image
+background = ImageObject("assets/Background.png")
+background.scale(WINDOW_WIDTH,WINDOW_HEIGHT)
+background.frectposition("center",WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
 
 #Player 
-player_image = pygame.image.load(join("assets","plane.png")).convert_alpha()
-player_image = pygame.transform.scale(player_image,(90,50))
-player_rect = player_image.get_frect(midleft = (10,WINDOW_HEIGHT / 2))
+player = ImageObject("assets/plane.png")
+player.scale(90,50)
+player.frectposition("midleft",10, WINDOW_HEIGHT/2)
 
-# Coins
-coin_image = pygame.image.load(join("assets", "coin.png")).convert_alpha()
-coin_image = pygame.transform.scale(coin_image, (35, 35))
-coin_positions = [(randint(0,WINDOW_WIDTH-100), randint(0,WINDOW_HEIGHT-200)) for i in range(7)]
+#Coins
+coin = ImageObject("assets/coin.png")
+coin.scale(35,35)
+coin_positions = [(randint(0,WINDOW_WIDTH-100),randint(0,WINDOW_HEIGHT-300)) for i in range(7)]
 
-# Main
+#Main
 while running:
     # Event Loop
     for event in pygame.event.get():
@@ -42,11 +43,15 @@ while running:
 
     # Game Elements
     display_surface.fill("Maroon") 
-    display_surface.blit(background_image, background_image_rect)
-    player_rect.left += 0.1
-    display_surface.blit(player_image, player_rect)
+    display_surface.blit(background.image, background.frect)
+
+    if player.frect.left > 1290: 
+        player.frect.left = 10
+    player.frect.left += 0.5
+
+    display_surface.blit(player.image, player.frect)
     for pos in coin_positions:
-        display_surface.blit(coin_image, pos)
+        display_surface.blit(coin.image, pos)
     pygame.display.update()
 
 pygame.quit()
