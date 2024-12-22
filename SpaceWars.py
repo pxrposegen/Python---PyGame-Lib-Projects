fcec6c4c4604f188420e815ccf51c3cf7dde301d
Genny <christo26thdec@gmail.com>
2024-12-22 20:06:46 +0530

    Added coins images into the game, Rescaled the Background Image to fit the Window Size

--- SpaceWars.py
+++ SpaceWars.py
@@ -9,7 +9,7 @@ WINDOW_HEIGHT = 720
 running = True
 
 # Creates a Window and Title
-display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT),pygame.RESIZABLE)
 pygame.display.set_caption("SpaceWars")
 
 # Icon
@@ -19,6 +19,11 @@ pygame.display.set_icon(icon_surface)
 
 # Background Image
 background_image = pygame.image.load(join("assets","Background.png")).convert_alpha()
+background_image = pygame.transform.scale(background_image,(1280,720))
+
+#Coins
+coin_image = pygame.image.load(join("assets","coin.png")).convert_alpha()
+coin_image = pygame.transform.scale(coin_image,(50,50))
 
 # Main
 while running: 
@@ -28,7 +33,8 @@ while running:
             running = False
 
     display_surface.fill("Maroon")
-    display_surface.blit(background_image,(-100,-100))
+    display_surface.blit(background_image,(0,0))
+    display_surface.blit(coin_image,(900,300))
     pygame.display.update()
 
 pygame.quit()   
