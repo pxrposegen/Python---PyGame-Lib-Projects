5ab18a9744fecbea3e22cf4910cbefbe64af13a8
Genny <christo26thdec@gmail.com>
2024-12-22 21:09:47 +0530

    Added FRect Functionality to Player Plane for better precision, Temporarily added Movement to Player Plane

--- SpaceWars.py
+++ SpaceWars.py
@@ -21,10 +21,16 @@ pygame.display.set_icon(icon_surface)
 # Background Image
 background_image = pygame.image.load(join("assets", "Background.png")).convert_alpha()
 background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+background_image_rect = background_image.get_frect(center = (WINDOW_WIDTH / 2,WINDOW_HEIGHT / 2))
+
+#Player 
+player_image = pygame.image.load(join("assets","plane.png")).convert_alpha()
+player_image = pygame.transform.scale(player_image,(90,50))
+player_rect = player_image.get_frect(midleft = (10,WINDOW_HEIGHT / 2))
 
 # Coins
 coin_image = pygame.image.load(join("assets", "coin.png")).convert_alpha()
-coin_image = pygame.transform.scale(coin_image, (50, 50))
+coin_image = pygame.transform.scale(coin_image, (35, 35))
 coin_positions = [(randint(0,WINDOW_WIDTH-100), randint(0,WINDOW_HEIGHT-200)) for i in range(7)]
 
 # Main
@@ -36,7 +42,9 @@ while running:
 
     # Game Elements
     display_surface.fill("Maroon") 
-    display_surface.blit(background_image, (0, 0))
+    display_surface.blit(background_image, background_image_rect)
+    player_rect.left += 0.1
+    display_surface.blit(player_image, player_rect)
     for pos in coin_positions:
         display_surface.blit(coin_image, pos)
     pygame.display.update()
