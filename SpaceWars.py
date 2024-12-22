99854a59473498587026fb87cdb71c6d54fa824b
Genny <christo26thdec@gmail.com>
2024-12-22 19:17:16 +0530

    Removed unnecessary assets, Implemented Window Icon

--- SpaceWars.py
+++ SpaceWars.py
@@ -1,17 +1,24 @@
 import pygame
 from sys import exit
 
-
 pygame.init()
 
+# Information regarding Window Size and Game State
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+running = True
 
 # Creates a Window and Title
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("SpaceWars")
-running = True
 
+# Icon
+icon_surface = pygame.image.load('assets/bronze.png')
+pygame.display.set_icon(icon_surface)
+# Surface
+
+
+# Main
 while running: 
     # Event Loop
     for event in pygame.event.get(): 
