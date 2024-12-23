aac7ffbfe03db6fb947a694ef1ac1d8cebbff658
Genny <christo26thdec@gmail.com>
2024-12-23 19:15:07 +0530

    Removed ImageObject Self-Defined Class, Re-implemented Player Movement with Sprite Class, Removed Coins temporarily

--- SpaceWars.py
+++ SpaceWars.py
@@ -1,74 +1,66 @@
 import pygame
 from os.path import join
-from random import randint
-from ImageObject import ImageObject
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("assets", "plane.png"))
+        self.image = pygame.transform.scale(self.image, (90, 50))
+        self.rect = self.image.get_frect(midleft=(10, WINDOW_HEIGHT / 2))
+        self.direction = pygame.math.Vector2()
+        self.speed = 300
+
+    def update(self, delta_time):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction = (
+            self.direction.normalize() if self.direction else self.direction
+        )
+        self.rect.center += self.direction * self.speed * delta_time
 
 pygame.init()
 
-# Information regarding Window Size and Game State
+# Window Constants, DO NOT CHANGE
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+
 running = True
 clock = pygame.time.Clock()
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-# Creates a Window and Title
-display_surface = pygame.display.set_mode(
-    (WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE
-)
-pygame.display.set_caption("SpaceWars")
-
-# Icon
-icon_path = join("assets", "bronze.png")
-icon_surface = pygame.image.load(icon_path).convert_alpha()
-pygame.display.set_icon(icon_surface)
+# Window Icon and Title
+icon_image = pygame.image.load(join("assets", "bronze.png")).convert_alpha()
+pygame.display.set_icon(icon_image)
+pygame.display.set_caption("SpaceWars.py")
 
 # Background Image
-background = ImageObject("assets/Background.png")
-background.scale(WINDOW_WIDTH, WINDOW_HEIGHT)
-background.frectposition("center", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+background_image = pygame.image.load(join("assets", "Background.png")).convert_alpha()
+background_image = pygame.transform.scale(
+    background_image, (WINDOW_WIDTH, WINDOW_HEIGHT)
+)
+background_image_rect = background_image.get_frect(
+    center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+)
 
-# Player
-player = ImageObject("assets/plane.png")
-player.scale(90, 50)
-player.frectposition("midleft", 10, WINDOW_HEIGHT / 2)
-player_direction = pygame.math.Vector2()
-player_speed = 300
+#Sprite Groups
+all_sprites = pygame.sprite.Group()
+player = Player(all_sprites)
 
-# Coins
-coin = ImageObject("assets/coin.png")
-coin.scale(35, 35)
-coin_positions = {
-    (randint(0, WINDOW_WIDTH - 100), randint(0, WINDOW_HEIGHT - 300)) for i in range(7)
-}
-
-# Main
 while running:
-    # Frame Rendering readjusted with Delta Time
-    delta = clock.tick(240) / 1000  # Return value converted from ms to s
+    delta = clock.tick(240) / 1000  # Return Value by default is ms, converted to s
 
     # Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Player Movement Input
-    keys = pygame.key.get_pressed()
-    player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-    player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+    all_sprites.update(delta)
 
-    # Diagonal player speed would increase, Consistent speed can be achieved with Normalizing
-    # if player_direction returns false if vector is [0,0] and true if it's any other value
-    player_direction = (
-        player_direction.normalize() if player_direction else player_direction
-    )
-    player.frect.center += player_direction * player_speed * delta
+    # Display loop
+    display_surface.fill((20, 40, 48))
+    display_surface.blit(background_image, background_image_rect)
+    all_sprites.draw(display_surface)
 
-    # Game Elements
-    display_surface.fill("Maroon")
-    display_surface.blit(background.image, background.frect)
-    for pos in coin_positions:
-        display_surface.blit(coin.image, pos)
-    display_surface.blit(player.image, player.frect)
     pygame.display.update()
-
 pygame.quit()
